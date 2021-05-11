ad7be96def9f8f586c0d67348e7078c9fb479459
hiroaki-inori <zanetti585@gmail.com>
2021-05-11 14:59:14 +0900

    新規登録・ログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :age_id, :gender_id, :occupation_id, :position, :prof_image, :pr])
+  end
 end
