ee74d90ca519f73c1e633991a7ad7c9b761c5ae2
hiroaki-inori <zanetti585@gmail.com>
2021-05-20 17:42:54 +0900

    背景画像等

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @comments = @user.comments.includes(:user)
+    @like_count = CommentLike.where(comment_id: @comments.ids).count
   end
 
   def edit
