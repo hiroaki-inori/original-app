137de6c0d79e421c6ba1fc0b663ac2f3b47dd877
hiroaki-inori <zanetti585@gmail.com>
2021-05-23 00:00:00 +0900

    コメント欄修正（非同期から同期に変更）

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,19 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = Comment.new(comment_params)
-    @user = @comment.user
+    # @user = @comment.user
+    @comment = Comment.create(comment_params)
     if @comment.save
-      ActionCable.server.broadcast 'comment_channel', comment: @comment, user: @user
+      redirect_to question_path(@comment.question)
+    else
+      @question = @comment.question
+      @comments = @question.comments.includes(:user)
+      @messages = @question.messages.includes(:question)
+      render "questions/show"
     end
+    # if @comment.save
+    #   ActionCable.server.broadcast 'comment_channel', comment: @comment, user: @user
+    # end
   end
 
   private
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :question
   has_many :comment_likes
+
+  validates :text, presence:true
 end
