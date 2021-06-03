5d038538a20ef88cef31829a0f16439125482ba8
hiroaki-inori <zanetti585@gmail.com>
2021-06-03 20:32:25 +0900

    ユーザープロフ画面修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @questions = Question.all
-    @comments = @user.comments.includes(:user)
-    # @like_count = CommentLike.where(comment_id: @comments.ids).count
     @my_questions = @user.questions.includes(:user)
-    @comment_questions = @questions.find(@comments.pluck(:question_id))
-    @comment_questions_count = @comment_questions.count
-    # binding.pry
+    comments = @user.comments.includes(:user)
+    @comment_questions = @questions.find(comments.pluck(:question_id))
+    @comment_questions_count = @comment_questions.count  
   end
 
   def edit
