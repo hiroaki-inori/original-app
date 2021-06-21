68eb7db4ed8365e39a25446a325771a4ce313500
hiroaki-inori <zanetti585@gmail.com>
2021-06-21 20:55:57 +0900

    question閲覧制限解除

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, :show]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @questions = Question.all
