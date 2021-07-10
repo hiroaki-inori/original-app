e0131bff8c05aef96d54937c94b65f1ee91f285e
hiroaki-inori <zanetti585@gmail.com>
2021-07-10 20:36:48 +0900

    スクロールボタン修正

--- app/javascript/jquery.js
+++ app/javascript/jquery.js
@@ -14,4 +14,33 @@ $(function() {
       $('.list').slideUp();
   });
 
+  //ページスクロールでtop移動
+  $('#page_top').on('click', function(){
+    $('html, body').animate({
+      'scrollTop':0
+    },500
+    );
+  });
+  //一定の位置まできたらスクロールボタン表示
+  if($('.scroll-btn-show').length){
+    var scrollBtnShow = $('.scroll-btn-show').offset().top;
+  }
+  var pageTop = $('#page_top');
+  $(window).scroll(function(){
+    if($(window).scrollTop() > scrollBtnShow){
+      pageTop.fadeIn();
+    }else{
+      pageTop.fadeOut();
+    }
+  });  
+  //「相談一覧」へスクロール移動
+  $('#show-btn').on('click', function(){
+    var id = $(this).attr('href');
+    // console.log(id);
+    var position = $(id).offset().top;
+    $('html, body').animate({
+      'scrollTop': position
+    }, 500);
+  });
+
 });
